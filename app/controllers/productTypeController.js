9e66e579950b4fd9786f9f9c8242c632219e1ed2
Trinh Bao Khuyen <khuyentb@devcamp.edu.vn>
2022-11-16 15:35:53 +0700

    first commit

---
+++ app/controllers/productTypeController.js
@@ -0,0 +1,126 @@
+const mongoose = require('mongoose');
+const productModel = require('../models/productModel');
+
+const productTypeModel = require("../models/productTypeModel");
+
+const createProductType = (req, res) => {
+    let body = req.body;
+    if (!body.name) {
+        return res.status(400).json({
+            message: `name is required`
+        })
+    }
+    let newProductType = {
+        _id: mongoose.Types.ObjectId(),
+        name: body.name,
+        description: body.description,
+    }
+    productTypeModel.create(newProductType, (error, data) => {
+        if (error) {
+            return res.status(500).json({
+                message: error.message
+            })
+        }
+        return res.status(201).json({
+            message: `Create successful`,
+            newProductType: data
+        })
+    })
+}
+
+//get all productType
+const getAllProductType = (req, res) => {
+    productTypeModel.find().populate('products')
+        .exec((error, data) => {
+            if (error) {
+                return res.status(500).json({
+                    message: error.message
+                })
+            }
+            return res.status(200).json({
+                message: ` get productType success`,
+                productType: data
+            })
+        })
+}
+
+
+//get productType by Id
+const getProductTypeById = (req, res) => {
+    let productTypeId = req.params.productTypeId;
+    if (!mongoose.Types.ObjectId.isValid(productTypeId)) {
+        return res.status(400).json({
+            message: `productTypeId is invalid`
+        })
+    }
+    productTypeModel.findById(productTypeId)
+        .populate('products')
+        .exec((error, data) => {
+            if (error) {
+                return res.status(500).json({
+                    message: error.message
+                })
+            }
+            return res.status(200).json({
+                message: ` get productType by Id success`,
+                productType: data
+            })
+        })
+
+}
+
+//update productType by Id
+const updateProductTypeById = (req, res) => {
+    let productTypeId = req.params.productTypeId;
+    let body = req.body;;
+    if (!mongoose.Types.ObjectId.isValid(productTypeId)) {
+        return res.status(400).json({
+            message: `productTypeId is invalid`
+        })
+    }
+    if (body.name !== undefined && body.name == "") {
+        return res.status(400).json({
+            message: `name is required`
+        })
+    }
+    let updateproductType = {
+        name: body.name,
+        description: body.description,
+    }
+    productTypeModel.findByIdAndUpdate(productTypeId, updateproductType, (error, data) => {
+        if (error) {
+            return res.status(500).json({
+                message: error.message
+            })
+        }
+        return res.status(200).json({
+            message: `Update successful`,
+            updateCourse: data
+        })
+    })
+}
+
+//delete by productType Id
+const deleteByProductTypeId = async (req,res) => {
+    let productTypeId = req.params.productTypeId;
+    if (!mongoose.Types.ObjectId.isValid(productTypeId)) {
+        return res.status(400).json({
+            message: `productTypeId is invalid`
+        })
+    }
+    await productModel.updateMany({type: productTypeId}, {type: null})
+    await productTypeModel.findByIdAndDelete(productTypeId, (error,data)=> {
+        if(error) {
+            return res.status(500).json({
+                message: error.message
+            })
+        }
+        return res.status(204).json({
+            message: `delete success`
+        })
+    })
+}
+
+
+
+module.exports = {createProductType, getAllProductType,getProductTypeById,updateProductTypeById,deleteByProductTypeId}
